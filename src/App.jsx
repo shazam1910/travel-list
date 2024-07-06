fa2c30bc0b6cf5e6b1d2000c1857d796a700f1fe
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-06 14:16:42 +0530

    Rendering list for travel app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
-  return <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
 function Logo() {
@@ -25,11 +33,32 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through " } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
-  return <footer className="stats">
-    <em> You have X items on ypur list, and you already packed X (X%)</em>
-  </footer>;
+  return (
+    <footer className="stats">
+      <em> You have X items on ypur list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
